19ec215181c6350596d44e049d868cc239e8b77b
smartmammal <alihas@gmail.com>
2015-03-19 20:41:24 +0000

    Initial commit

---
+++ bicycle.py
@@ -0,0 +1,29 @@
+class Bicycle(object):
+  def __init__(self, model, weight, cost):
+    self.model = model
+    self.weight = weight
+    self.cost = cost
+
+class Shop(object):
+  def __init__(self, name, inventory, margin=20%):
+    self.name = model
+    self.inventory = inventory
+    self.margin = margin
+    self.profit = 0
+    
+  def funcnom(self, var1, var2):
+    #Add bike into inventory
+    #Sell bike
+    #Print inventory for each bike it carries
+    #Print profit
+    #Print affordable bikes given customer
+  
+class Customer(object):
+  def __init__(self, model, funds=0):
+    self.name = model
+    self.funds = funds
+    self.inventory = []
+    
+  def funcnom(self, var1, var2):    
+    #Buy a bike, name of purchase, cost, subtract from funds
+    #money left in funds
